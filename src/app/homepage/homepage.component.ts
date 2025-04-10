ba50062e2fb9497b1ea0249bb3a9415dadbbd1b9
luca-andrei51003 <luca.culcean@gmail.com>
2025-04-10 14:24:55 +0300

    intialized homepage logic

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
+
+@Component({
+  selector: 'app-weather-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    ChartModule
+  ],
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class WeatherHomeComponent {
+  chartData = {
+    labels: ['13 Mar', '14 Mar', '15 Mar', '16 Mar', '17 Mar', '18 Mar', '19 Mar', '20 Mar'],
+    datasets: [
+      {
+        label: 'Temperature',
+        data: [12, 24, 18, 27, 20, 10, 5, 15],
+        fill: false,
+        borderColor: '#42A5F5',
+        tension: 0.4
+      }
+    ]
+  };
+}
