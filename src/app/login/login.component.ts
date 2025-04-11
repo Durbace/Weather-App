2ae4244b892fa5477d64df48e07e3d6a4dfaf84b
Erik <doodletvm@gmail.com>
2025-04-11 19:11:14 +0300

    improved look and adapted to PrimeNG

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { onAuthStateChanged, Auth } from '@angular/fire/auth';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+
+import { CardModule } from 'primeng/card';
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, InputTextModule, PasswordModule, ButtonModule, MessagesModule, MessageModule, CardModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
@@ -33,6 +42,12 @@ export class LoginComponent {
   onLogin() {
     this.auth.login(this.email, this.password)
       .then(() => this.router.navigate(['/']))
-      .catch(err => this.error = err.message);
+      .catch(err => {
+        if (err.code) {
+          this.error = 'Wrong email or password, Please try again.';
+        }
+
+        setTimeout(() => this.error = '', 15000);
+      });
   }
 }
