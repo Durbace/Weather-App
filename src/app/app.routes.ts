07b5a0d5470a2b618f4b28c66945cb07cfca941e
luca-andrei51003 <luca.culcean@gmail.com>
2025-04-23 11:22:34 +0300

    resolved merge conflict
    routes.ts encountered merge error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,20 @@ import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
 
+<<<<<<< Updated upstream
   { path: 'home', component: WeatherHomeComponent },
   //{ path: 'test', component: SearchCityComponent },
   //{ path: '', component: SearchCityComponent }, 
   { path: 'home/:name', component: CityDetailsComponent },
   { path: 'favorites', component: FavoriteCitiesComponent },
+=======
+  { path: 'home', component: HomepageComponent, canActivate: [authGuard] },
+  { path: 'test', component: SearchCityComponent, canActivate: [authGuard] },
+  //{ path: '', component: SearchCityComponent }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'favorites', component: FavoriteCitiesComponent, canActivate: [authGuard] },
+  { path: 'city/:name', component: CityDetailsComponent, canActivate: [authGuard] }
+>>>>>>> Stashed changes
   //{ path: 'test_page', component: WeatherHomeComponent}
 ];
